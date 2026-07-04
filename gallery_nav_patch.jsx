ccfbc204b5452955bde4a16d9e7b44a6f31f48e5
untalbetoski <bodly28@gmail.com>
2026-07-04 00:08:13 -0600

    Load functional contacts module

--- gallery_nav_patch.jsx
+++ gallery_nav_patch.jsx
@@ -24,6 +24,7 @@
   function addRoute() {
     window.ROUTES = window.ROUTES || {};
     window.ROUTES.galeria = { c: 'GaleriaComunicacion', crumb: ['Comunicación', 'Galería'] };
+    window.ROUTES.contactos = { c: 'Contactos', crumb: ['CRM', 'Contactos'] };
     const nav = window.NAV || [];
     nav.forEach(sec => (sec.items || []).forEach(it => {
       if (it.id === 'comunicacion-fam') {
@@ -34,5 +35,6 @@
   }
   loadScriptOnce('student_documents_label_patch.js?v=20260703-logo-no-signature');
   forceLoadBabel('views_crm.jsx?v=20260704-real-only');
+  forceLoadBabel('views_contactos.jsx?v=20260704-functional-directory');
   addRoute();
 })();
